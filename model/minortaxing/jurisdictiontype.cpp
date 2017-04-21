d672e80c74cc93d6b63ba1a7026df443b9f1b18d
David Mahoney <dmahoney@rdffg.bc.ca>
2017-04-21 14:56:19 -0700

    Split minor tax jurisdiction type labels into words

--- model/minortaxing/jurisdictiontype.cpp
+++ model/minortaxing/jurisdictiontype.cpp
@@ -4,6 +4,19 @@
 #include <QDjangoQuerySet.h>
 
 using namespace model::minortaxing;
+
+QString spacify(QString label)
+{
+   QStringList resList;
+   QRegularExpression rex("([A-Z][a-z]+)");
+   auto i = rex.globalMatch(label);
+   while (i.hasNext())
+   {
+       resList << i.next().captured(1);
+   }
+   return resList.join(" ");
+}
+
 JurisdictionType::JurisdictionType(QObject *parent) : QDjangoModel(parent) {}
 
 JurisdictionType::TaxingJurisdictionType JurisdictionType::type() const { return m_type; }
@@ -28,14 +41,14 @@ void JurisdictionType::populate()
     int index = mo.indexOfEnumerator("TaxingJurisdictionType");
     QMetaEnum metaEnum = mo.enumerator(index);
     QDjangoQuerySet<JurisdictionType> query;
-    if (query.count() < metaEnum.keyCount())
+   // if (query.count() < metaEnum.keyCount())
     {
         for (int i = 0; i < metaEnum.keyCount(); i++)
         {
             JurisdictionType prop;
             prop.setType(static_cast<TaxingJurisdictionType>(metaEnum.value(i)));
             prop.setDescription(metaEnum.valueToKey(prop.type()));
-            qDebug() << "Jurisdiction: " << static_cast<int>(prop.type()) << ", " << prop.description();
+            qDebug() << "Jurisdiction: " << static_cast<int>(prop.type()) << ", " << spacify(prop.description());
             if (!prop.save())
                 throw SaveError(prop.lastError().text());
         }
